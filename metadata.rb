909c6c985e0992d49e69cb53d322025318fff3f4
Seth Griffin <griffinseth@yahoo.com>
2017-09-16 15:10:53 -0500

    Incremented version no

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'phpvagrantmulti@gmail.com'
 license          'GPL v3'
 description      'Installs/Configures php_myadmin for PhpVagrantMulti'
 long_description 'Installs/Configures php_myadmin for PhpVagrantMulti'
-version          '0.1.1'
+version          '0.1.2'
 
